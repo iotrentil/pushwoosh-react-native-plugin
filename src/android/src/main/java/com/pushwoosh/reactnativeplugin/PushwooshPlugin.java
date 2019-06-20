c4f18caa88edc4facd4b15a01219a8b7b020ce48
leehch0 <leehch0@gmail.com>
2019-06-20 22:45:13 +0900

    for androidx

--- src/android/src/main/java/com/pushwoosh/reactnativeplugin/PushwooshPlugin.java
+++ src/android/src/main/java/com/pushwoosh/reactnativeplugin/PushwooshPlugin.java
@@ -8,7 +8,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.LifecycleEventListener;
